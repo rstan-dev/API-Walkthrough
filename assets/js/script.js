aacede1c06e7fcc1494ac707ab308dd3a57f0eeb
Russ Smith <rstanleym74@gmail.com>
2023-05-19 15:47:18 +0100

    feat: fetch api status details and display in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,28 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) { 
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your API Key is valid until:</div>`;
+    results += `<div>${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
